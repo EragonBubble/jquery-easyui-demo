eca0ae6399fbc90f0881b90712e58e6a50ec8005
longwang <896489037@qq.com>
2018-08-01 22:57:49 +0800

    update css

--- js/js-common.js
+++ js/js-common.js
@@ -2,10 +2,16 @@ $('.js-ui-menu-close-icon').click(function(){
 	var menuWidth = $('.js-ui-menu').width();
 	console.log("[.js-ui-menu] width ", menuWidth);
 	if (menuWidth < 100) {
-		$('.js-ui-menu').animate({'width': '138px'});
-		$('.js-ui-menu-close-icon').animate({'left': '138px'});
+		$('.js-ui-menu').animate({'width': '145px'});
+		$('.js-ui-menu-close-icon').animate({'left': '155px'});
+		$('.js-ui-menu-close-icon').text('<');
+		$('.js-ui-logo').fadeIn();
+		$('.js-ui-menu-title').fadeIn();
 	} else {
 		$('.js-ui-menu').animate({'width': '28px'});
-		$('.js-ui-menu-close-icon').animate({'left': '28px'});
+		$('.js-ui-menu-close-icon').animate({'left': '38px'});
+		$('.js-ui-menu-close-icon').text('>');
+		$('.js-ui-logo').fadeOut();
+		$('.js-ui-menu-title').fadeOut();
 	}
 })
