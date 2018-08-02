e03d26bfd2a0cfccf67fa02e8a4594fd6ab6c44c
longwang <896489037@qq.com>
2018-08-02 10:08:10 +0800

    add font-awesome

--- js/js-common.js
+++ js/js-common.js
@@ -9,12 +9,14 @@ $('.js-ui-menu-close-icon').click(function(){
 		$('.js-ui-menu').animate({'width': '145px'});
 		$('.js-ui-menu-close-icon').animate({'left': '155px'});
 		$('.js-ui-menu-close-icon').text('<');
+		$('.js-ui-container').animate({'margin-left': '155px'});
 		$('.js-ui-logo').fadeIn();
 		$('.js-ui-menu-title').fadeIn();
 	} else {
 		$('.js-ui-menu').animate({'width': '28px'});
 		$('.js-ui-menu-close-icon').animate({'left': '38px'});
 		$('.js-ui-menu-close-icon').text('>');
+		$('.js-ui-container').animate({'margin-left': '38px'});
 		$('.js-ui-logo').fadeOut();
 		$('.js-ui-menu-title').fadeOut();
 	}
